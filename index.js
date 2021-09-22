f25542de27c3d71f6ef9c07dbdf17cc13054d608
ingridhadibrata <ingridhadibrata@yahoo.com>
2021-09-23 04:18:23 +0700

    mini challenge 6-2

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const { User } = require('./models')
 const port = 3000
 
-app.use(express.json())
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended:false
+}))
 
 app.get('/users', (req,res) => {
     User.findAll()
@@ -21,6 +24,36 @@ app.post('/users', (req,res) => {
         res.send('New user has been made')
     })
 })
+
+app.get('/users/delete/:id', (req, res) => {
+    User.destroy({ where: {id: req.params.id} })
+    .then(()=> {
+        res.send('User Game has been deleted')
+    })
+})
+
+app.get('/users/update/:id', (req, res) => {
+    User.findOne({ where: {id: req.params.id} })
+    .then((user)=> {
+        res.render('user/update', {
+            user
+        })
+    })
+})
+
+app.post('/users/update/:id', (req, res) => {
+    User.update({
+        username: req.body.username,
+        password: req.body.password
+    },
+    { where: { id: req.params.id } }
+    )
+    .then(() => {
+        res.send('User Game has been updated')
+    })
+})
+
+
 app.listen(port, () => {
     console.log("It Worked!");
 });
